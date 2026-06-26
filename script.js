63af961c235fa68e711ca6e78acf5aa383d72e1d
zaid-portfolio <zk034300@gmail.com>
2026-06-26 00:20:04 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function showMessage(){
+
+    alert("Welcome to Zaid Khan Portfolio 🚀");
+
+}
+function showMessage(){
+
+    alert("Welcome to Zaid Khan Portfolio 🚀");
+
+}
+
+
+function darkMode(){
+
+    document.body.classList.toggle("light");
+
+}
+window.onload = function(){
+
+    document.body.style.opacity = "1";
+
+}
